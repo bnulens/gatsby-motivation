c894283e6cd92503ec85d6ec8fc904ff9d86ba82
Brecht <brechtnulens@gmail.com>
2020-08-25 17:49:44 +0200

    Initial setup and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Motivation Page`,
+    description: `A customised motivation of why you should hire me.`,
+    author: `Brecht Nulens`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
