ffe2864bb62c60eb21080859d2fdd7694e94e069
Brecht <brechtnulens@gmail.com>
2020-08-26 18:04:52 +0200

    Added resolve for react-svg images

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /icons/, // See below to configure properly
+        },
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
@@ -26,7 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profile-image.png`, // This path is relative to the root of the site.
       },
     },
     {
