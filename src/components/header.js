cfc64aaa25dcb61ede9fcbd58cdc333196376724
Brecht <brechtnulens@gmail.com>
2020-08-26 18:06:35 +0200

    Made a styled header with custom shape

--- src/components/header.js
+++ src/components/header.js
@@ -3,23 +3,43 @@ import PropTypes from "prop-types"
 import styled from "styled-components"
 import HeaderNav from "./navbar/Navbar"
 
+const StyledHeader = styled.header`
+  position: relative;
+  margin-bottom: 24px;
+  ::before {
+    content: "";
+    position: absolute;
+    background: #ADFFCE;
+    width: 100%;
+    height: 52px;
+    -webkit-clip-path: polygon(0 0, 100% 0,100% 40%, 0 100%);
+    clip-path: polygon(0 0, 100% 0%, 100% 40%, 0 100%);
+    z-index: -5;
+  }
+`
+
 const MainTitle = styled.h1`
-  display: block;
-  color: rebeccapurple;
-  width: 100%;
-  span {
-    margin: 0 6px;
+  width: 45%;
+  font-size: 28px;
+  padding: 6px;
+  .title-first-name {
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+  .title-last-name {
+    font-weight: 100;
+    text-transform: uppercase;
   }
 `
 
 const Header = () => (
-  <header>
+  <StyledHeader>
     <MainTitle>
-      <span>Brecht</span>
-      <span>Nulens</span>
+      <span className="title-first-name">Brecht</span>
+      <span className="title-last-name">Nulens</span>
     </MainTitle>
     <HeaderNav/>
-  </header>
+  </StyledHeader>
 )
 
 Header.propTypes = {
