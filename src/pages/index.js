39b79a868da96f01215ee9a3f05ccf571b97f50f
Brecht <brechtnulens@gmail.com>
2020-09-03 17:48:56 +0200

    Blockquote is a div - added ToggleNightMode to header comp - assigned classnames for darkmode styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react'
+import styled from 'styled-components'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import ToggleNightMode from '../components/ToggleNightMode'
 
 const MainWrapper = styled.section`
 `
 const StyledGreeting = styled.h2`
   font-weight: 300;
 `
-const BlockQuote = styled.span`
+const BlockQuote = styled.div`
   display: flex;
   align-items: center;
   height: 60px;
@@ -80,18 +81,19 @@ const IndexPage = () => (
       keywords={[`profile`, `presentation`, `Brecht`, `developer`]}
     />
     <MainWrapper>
+      <ToggleNightMode/>
       <StyledGreeting>Dag Josiane!</StyledGreeting>
-      <BlockQuote><p>...en aan ieder die dit leest</p></BlockQuote>
+      <BlockQuote><p className="quote-text">...en aan ieder die dit leest</p></BlockQuote>
         <MainContent>
           <Chapter>
-            <SubTitle>Wie ben ik?</SubTitle>
+            <SubTitle className="sub-title">Wie ben ik?</SubTitle>
             <p>Ik ben <ExternalLink href="https://brechtnulens.be" target="__blank" rel="noopener norefferrer">Brecht Nulens</ExternalLink>, 25 jaar oud en een developer uit Hasselt.</p>
             <p>Mijn interesse voor development komt voort uit mijn opleiding Grafisch Ontwerp aan de PXL waar we met Dreamweaver en jQuery simpele layouts maakten voor denkbeeldige bedrijven. Zo raakte ik verkocht aan de wereld van webdesign en development en verliet ik vroegtijdig de schoolbank.</p>
             <p>Via een omweg als netwerkbeheerder ben ik terecht gekomen bij BeCode. Een initiatief waarbij ik enorm veel heb opgestoken en even geëngageerde studenten heb leren kennen.</p>
             <DescriptionSpan>Het BeCode team, ik zit in het midden!</DescriptionSpan>
           </Chapter>
           <Chapter>
-            <SubTitle>Waarom ik?</SubTitle> 
+            <SubTitle className="sub-title">Waarom ik?</SubTitle> 
             <p>Voordat ik begon aan mijn avontuur had ik al wat kennis over netwerken en Linux (Debian & Ubuntu). Zo had ik meer vertrouwen om in de richting van developer te gaan.
             </p>
             <p>Sinds ik met development begonnen ben, heb ik toch al van redelijk wat zaken kunnen proeven. Zo maakte ik onder andere <ExternalLink href="https://brechtnulens.be" target="__blank" rel="noopener norefferrer">mijn portfolio website</ExternalLink> met Vue.js. Daarnaast, een voorbeeld site voor een platenwinkel in Genk <ExternalLink href="https://epic-galileo-8a7f26.netlify.app/" target="__blank" rel="noopener norefferrer">“George & the Bear”</ExternalLink>, met React en <ExternalLink href="https://gatsby.com" target="__blank" rel="noopener norefferrer">Gatsby</ExternalLink>, één van de static site generators en de <ExternalLink href="https://jamstack.org/" target="__blank" rel="noopener norefferrer">JAMstack</ExternalLink> frameworks.</p>
