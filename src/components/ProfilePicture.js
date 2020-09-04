8f1e24ba2d1837a888685eea13299f1961025633
Brecht <brechtnulens@gmail.com>
2020-09-04 18:31:09 +0200

    Changed profile picture and styling

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -5,12 +5,15 @@ import styled from "styled-components"
 const StyledProfilePicture = styled(Image)`
   display: block;
   position: absolute;
-  border: 4px solid white;
-  border-radius: 50%;
   top: 15px;
   right: 0;
   width: 110px;
   height: 110px;
+  background: #41B883;
+  border: 4px solid white;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: 100% 30%;
   -webkit-box-shadow: 0px 9px 35px -14px rgba(66,66,66,0.75);
   -moz-box-shadow: 0px 9px 35px -14px rgba(66,66,66,0.75);
   box-shadow: 0px 9px 35px -14px rgba(66,66,66,0.75);
@@ -24,7 +27,7 @@ const ProfilePicture = () => {
     <>
       <StyledProfilePicture 
         cloudName="bnulens"
-        publicId="gatsby-motivation/profile_picture.png"
+        publicId="gatsby-motivation/profile_picture_original.png"
         secure="true"
         alt="brecht-nulens-profile"
       />
