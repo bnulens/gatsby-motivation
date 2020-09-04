6e31e5ed3c91c594674709521ee3702aa878cd44
Brecht <brechtnulens@gmail.com>
2020-09-04 18:32:00 +0200

    New group photo and projectimages added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Image } from 'cloudinary-react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import ExamplePicture from '../components/ExampleImage'
 
 const MainWrapper = styled.section`
 `
@@ -20,6 +22,7 @@ const BlockQuote = styled.div`
   }
 `
 const DescriptionSpan = styled.span`
+  display: block;
   font-size: 12px;
   font-weight: 200;
 `
@@ -73,6 +76,18 @@ const Chapter = styled.article`
     margin-bottom: 24px;    
   }
 `
+const GroupImage = styled(Image)`
+  display: block;
+  height: 240px;
+  width: 100%;
+  margin: 6px auto;
+  border-radius: 25px;
+  object-fit: cover;
+  object-position: 50% 25%;
+  @media screen and (min-width: 900px) {
+    height: 400px;
+  } 
+`
 const IndexPage = () => (
   <Layout>
     <SEO 
@@ -89,13 +104,19 @@ const IndexPage = () => (
             <p>Mijn interesse voor development komt voort uit mijn opleiding Grafisch Ontwerp aan de PXL waar we met Dreamweaver en jQuery simpele layouts maakten voor denkbeeldige bedrijven. Zo raakte ik verkocht aan de wereld van webdesign en development en verliet ik vroegtijdig de schoolbank.</p>
             <p>Via een omweg als netwerkbeheerder ben ik terecht gekomen bij BeCode. Een initiatief waarbij ik enorm veel heb opgestoken en even geëngageerde studenten heb leren kennen.</p>
             <DescriptionSpan>Het BeCode team, ik zit in het midden!</DescriptionSpan>
+            <GroupImage
+              cloudName="bnulens"
+              publicId="gatsby-motivation/becode_hopper.jpg"
+              secure="true"
+              alt="becode-hopper"
+            />
           </Chapter>
           <Chapter>
             <SubTitle className="sub-title">Waarom ik?</SubTitle> 
             <p>Voordat ik begon aan mijn avontuur had ik al wat kennis over netwerken en Linux (Debian & Ubuntu). Zo had ik meer vertrouwen om in de richting van developer te gaan.
             </p>
             <p>Sinds ik met development begonnen ben, heb ik toch al van redelijk wat zaken kunnen proeven. Zo maakte ik onder andere <ExternalLink href="https://brechtnulens.be" target="__blank" rel="noopener norefferrer">mijn portfolio website</ExternalLink> met Vue.js. Daarnaast, een voorbeeld site voor een platenwinkel in Genk <ExternalLink href="https://epic-galileo-8a7f26.netlify.app/" target="__blank" rel="noopener norefferrer">“George & the Bear”</ExternalLink>, met React en <ExternalLink href="https://www.gatsbyjs.com/" target="__blank" rel="noopener norefferrer">Gatsby</ExternalLink>, één van de static site generators en de <ExternalLink href="https://jamstack.org/" target="__blank" rel="noopener norefferrer">JAMstack</ExternalLink> frameworks.</p>
-            <span>**here comes the example image**</span>
+            <ExamplePicture/>
             <StyledUnorderedList>
               <StyledListItem>Vue.js</StyledListItem>
               <StyledListItem>React.js, Gatsby.js & styled components</StyledListItem>
